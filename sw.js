17062de092cae7ce000a4eda6c87eb381d84abf2
lukasw2009 <lukas.weichselberger@icloud.com>
2026-09-10 21:27:20 +0200

    Refresh offline cache for v10

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE='schnapsen-clean-v9-20260910';
+const CACHE='schnapsen-clean-v10-20260910';
 const ASSETS=['./','./index.html','./style.css','./app.js','./pwa.js','./manifest.webmanifest','./icon.svg'];
 self.addEventListener('install',event=>event.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS))));
 self.addEventListener('activate',event=>event.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k)))).then(()=>self.clients.claim())));
