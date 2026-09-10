967bbcb1bd8d4b0a4c21a122bd209c8f886285b8
lukasw2009 <lukas.weichselberger@icloud.com>
2026-09-10 20:20:05 +0200

    Cache dealer rotation update

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
-const CACHE='schnapsen-v6-20260910';
-const ASSETS=['./','./index.html','./style.css','./app-core.js','./app-ui.js','./upgrade-ui.js','./upgrade-game-history.js','./upgrade-players-stats.js','./upgrade-rules-backup.js','./upgrade-bind.js','./rules-v6.js','./v6.css','./pwa.js','./manifest.webmanifest','./icon.svg'];
+const CACHE='schnapsen-v7-20260910';
+const ASSETS=['./','./index.html','./style.css','./app-core.js','./app-ui.js','./upgrade-ui.js','./upgrade-game-history.js','./upgrade-players-stats.js','./upgrade-rules-backup.js','./upgrade-bind.js','./rules-v6.js','./dealer-v7.js','./v6.css','./pwa.js','./manifest.webmanifest','./icon.svg'];
 self.addEventListener('install',event=>event.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS))));
 self.addEventListener('activate',event=>event.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k)))).then(()=>self.clients.claim())));
 self.addEventListener('message',event=>{if(event.data?.type==='SKIP_WAITING')self.skipWaiting()});
