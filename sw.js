d9eb321dabcf381dcfd824aa1fc82a8de08f0fd5
lukasw2009 <lukas.weichselberger@icloud.com>
2026-09-11 07:04:58 +0200

    Refresh cache for scoring and statistics fixes

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE='schnapsen-clean-v11-20260911';
+const CACHE='schnapsen-clean-v12-20260911';
 const ASSETS=['./','./index.html','./style.css','./app.js','./history-controls.js','./pwa.js','./manifest.webmanifest','./icon.svg'];
 self.addEventListener('install',event=>event.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS))));
 self.addEventListener('activate',event=>event.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k)))).then(()=>self.clients.claim())));
